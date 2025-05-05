3d39a8fd6eb2e3cae8382afee7ec9cadb8935455
zainab <zainab.zulfiqar@gmail.com>
2025-05-05 14:06:46 +0500

    Modified backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "ghjkl-bucket"
     key            = "key/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-west-3"
   }
 }
